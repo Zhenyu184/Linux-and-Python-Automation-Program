65a9575891ba826229900d064b914f1f9e1d10d5
chris <zyy1120126@gmail.com>
2021-02-26 06:59:00 -0800

    3 commit

---
+++ Install_python.sh
@@ -0,0 +1,17 @@
+python3 --version
+
+sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev wget libbz2-dev
+
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get -y update
+sudo dnf install -y python3
+sudo apt install -y python3-pip
+
+sudo add-apt-repository universe
+sudo apt -y update 
+sudo apt install -y python2
+sudo apt install -y curl
+curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+
+
---
+++ linux_initialization.sh
@@ -0,0 +1,35 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install build-essential
+sudo apt-get -y install git mercurial
+sudo apt-get -y install make cmake
+sudo apt-get -y install net-tools
+sudo apt-get -y install tree
+sudo apt-get -y install vim
+sudo apt-get -y install ntfs-3g
+sudo apt-get -y install synaptics
+sudo apt-get -y install wget curl
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
