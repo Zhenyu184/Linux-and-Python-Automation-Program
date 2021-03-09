ed52a5285857ec5676244c4cf432ea99c5fb59f6
Auto_Push_Bot <zyy1120126@gmail.com>
2021-03-08 18:55:14 -0800

    Auto_Push_Bot

---
+++ Auto_Push_To_Git.sh
@@ -0,0 +1,20 @@
+git --version
+git config --global user.name "Auto_Push_Bot"
+git config --global user.email "<zyy1120126@gmail.com>"
+
+git add .
+git commit -m "Auto_Push_Bot"
+
+
+git push
+
+
+
+
+
+
+
+
+
+
+
